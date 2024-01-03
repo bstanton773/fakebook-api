3030230df89386bb4b8d1402ff0d9915f2e56aab
Brian <bstanton773@gmail.com>
2024-01-02 23:06:21 -0600

    Add Post model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from datetime import datetime, timedelta
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
@@ -10,6 +10,7 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,7 +18,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id}|{self.username}>"
-    
+
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -29,6 +30,29 @@ class User(db.Model):
     def set_password(self, password):
         self.password = generate_password_hash(password)
         self.save()
-    
+
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Post {self.id}|{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
